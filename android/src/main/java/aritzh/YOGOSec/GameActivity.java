69eb678f46e4716a2f7713eef48a123521b798ca
aritzhack <aritzhack@gmail.com>
2013-12-27 22:25:55 +0100

    * Moved android classes to aritzh.YOGOSec * Added actual icons for the game, although they may well be temporary * Set the game name to YOGOSec * Reformatted

---
+++ android/src/main/java/aritzh/YOGOSec/GameActivity.java
@@ -0,0 +1,17 @@
+package aritzh.YOGOSec;
+
+import YOGOSec.core.Game;
+import android.os.Bundle;
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+public class GameActivity extends AndroidApplication {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        config.useGL20 = true;
+        initialize(Game.INSTANCE, config);
+    }
+}
