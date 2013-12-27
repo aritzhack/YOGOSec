b9ad046d1ac16a108f9bac302396ec33a0fd8f94
aritzhack <aritzhack@gmail.com>
2013-12-27 22:26:53 +0100

    + Added icons for the desktop too (x128, x32 and x16) * Set window title to "YOGOSec"

--- desktop/src/main/java/YOGOSec/java/GameDesktop.java
+++ desktop/src/main/java/YOGOSec/java/GameDesktop.java
@@ -1,5 +1,6 @@
 package YOGOSec.java;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
@@ -9,6 +10,10 @@ public class GameDesktop {
 	public static void main (String[] args) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.useGL20 = true;
+        config.title = "YOGOSec";
+        config.addIcon("assets/icon128.png", Files.FileType.Internal);
+        config.addIcon("assets/icon32.png", Files.FileType.Internal);
+        config.addIcon("assets/icon16.png", Files.FileType.Internal);
 		new LwjglApplication(Game.INSTANCE, config);
 	}
 }
